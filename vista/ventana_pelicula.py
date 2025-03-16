ed4031dd6ec54c46dce20f944f50bd4bf5b1d94a
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2025-03-16 15:38:08 -0300

    Corregido carga de los posters

--- vista/ventana_pelicula.py
+++ vista/ventana_pelicula.py
@@ -1,27 +1,28 @@
-import os
 from PySide6.QtWidgets import QDialog
 from PySide6.QtGui import QPixmap
 from ui.datos_pelicula import Ui_Dialog
+from modelo import Pelicula
 
 
 class VentanaPelicula(QDialog):
-    def __init__(self, pelicula):
+    def __init__(self):
         super().__init__()
         self.__ui = Ui_Dialog()
         self.__ui.setupUi(self)
         self.setWindowTitle("Detalles de la Película")
 
+    def mostrar_datos(self, pelicula):
         self.__ui.label_titulo_ingresado.setText(pelicula["titulo"])
         self.__ui.label_sinopsis.setText(pelicula["sinopsis"])
         self.__ui.label_puntuacion.setText(str(pelicula["puntuacion"]))
         self.__ui.label_actores.setText(", ".join(pelicula["actores"]))
-        self.__cargar_poster(pelicula['poster'])
+        self.__modelo_pelicula = Pelicula()
+        self.__cargar_ruta = self.__modelo_pelicula.cargar_ruta(pelicula["poster"])
+        self.__cargar_poster(self.__cargar_ruta)
 
-    def __cargar_poster(self, archivo):
-        ruta = os.path.join("peliculas", "imagen", archivo)
+    def __cargar_poster(self, ruta):
         pixmap = QPixmap(ruta)
-
         if pixmap:
             self.__ui.label_poster.setPixmap(pixmap.scaled(300, 450))
         else:
-            print("Error al cargar la imagen: archivo no encontrado.")
+            print("Error al cargar la imagen: poster no encontrado.")
