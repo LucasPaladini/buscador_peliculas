eb290182bde87d49080a8a5d33e427fb978fa3ef
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2025-03-16 15:37:42 -0300

    Añadido las signals para conectar con los métodos del modelo

--- vista/ventana_actores.py
+++ vista/ventana_actores.py
@@ -1,15 +1,18 @@
 from PySide6.QtWidgets import QDialog, QTableWidgetItem, QMessageBox
+from PySide6.QtCore import Signal
 from ui.busqueda_actores import Ui_dialog_actor
-from vista.ventana_pelicula import VentanaPelicula
 
 
 class VentanaActor(QDialog):
-    def __init__(self, peliculas):
+    buscar_actor = Signal(str)
+    busqueda_por_actores = Signal(str, str)
+    abrir_pelicula = Signal(str)
+
+    def __init__(self):
         super().__init__()
         self.__ui = Ui_dialog_actor()
         self.__ui.setupUi(self)
         self.setWindowTitle("Ventana Actores")
-        self.__peliculas = peliculas
 
         # self.resize(500, 600)
 
@@ -25,59 +28,36 @@ class VentanaActor(QDialog):
         self.__ui.table_actores.verticalHeader().setVisible(False)
         self.__ui.table_actores.horizontalHeader().setStretchLastSection(True)
 
-        self.cargar_actores()
 
-    def __buscar_actor(self):
-        texto_busqueda = self.__ui.line_ingreso_actor.text().strip().lower()
+    def emitir_buscar_actor(self):
+        titulo = self.__ui.line_ingreso_actor.text().lower().strip()
+        self.buscar_actor.emit(titulo)
+
+    def __obtener_nombres_actores(self):
+        actor_1 = self.__ui.actor_n1.text().strip().lower()
+        actor_2 = self.__ui.actor_n2.text().strip().lower()
+        return actor_1, actor_2
+
+    def emitir_busqueda_por_actores(self):
+        actor_1, actor_2 = self.__obtener_nombres_actores()
+        self.busqueda_por_actores.emit(actor_1, actor_2)
+
+    def mostrar_error(self, error):
+        QMessageBox.warning(self, "Error", error)
+
+    def cargar_actores(self, actores):
+        self.__completar_tabla(actores)
+
+    def __completar_tabla(self, actores):
         self.__ui.table_actores.setRowCount(0)
 
-        for pelicula in self.__peliculas:
-            for actor in pelicula["actores"]:
-                if texto_busqueda in actor.lower():
-                    fila = self.__ui.table_actores.rowCount()
-                    self.__ui.table_actores.insertRow(fila)
-                    self.__ui.table_actores.setItem(fila, 0, QTableWidgetItem(actor))
-
-        if self.__ui.table_actores.rowCount() == 0:
-            QMessageBox.warning(self, "Error", "No se encontró actor con ese nombre.")
-
-    def cargar_actores(self):
-        actores = []
-
-        for pelicula in self.__peliculas:
-            for actor in pelicula["actores"]:
-                if actor not in actores:
-                    actores.append(actor)
-
-        for actor in sorted(actores):
+        for actor in actores:
             fila = self.__ui.table_actores.rowCount()
             self.__ui.table_actores.insertRow(fila)
             self.__ui.table_actores.setItem(fila, 0, QTableWidgetItem(actor))
 
-    def __obtener_nombres_actores(self):
-        actor_1 = self.__ui.actor_n1.text().strip()
-        actor_2 = self.__ui.actor_n2.text().strip()
-        return actor_1, actor_2
-
-    def __buscar_pelicula_por_actores(self):
-        actor_1, actor_2 = self.__obtener_nombres_actores()
-
-        if actor_1 and actor_2:
-            if actor_1 != actor_2:
-                peliculas_encontradas = []
-                for pelicula in self.__peliculas:
-                    actores = [actor.lower() for actor in pelicula['actores']]
-                    if actor_1.lower() in actores and actor_2.lower() in actores:
-                        peliculas_encontradas.append(pelicula)
-
-                if peliculas_encontradas:
-                    self.__mostrar_peliculas_encontradas(peliculas_encontradas)
-                else:
-                    QMessageBox.warning(self, "Error", "No se encontraron películas con esos actores.")
-            else:
-                QMessageBox.warning(self, "Error", "Los actores no pueden ser los mismos.")
-        else:
-            QMessageBox.warning(self, "Error", "Ingrese ambos nombres de actores.")
+    def cargar_peliculas(self, peliculas):
+        self.__mostrar_peliculas_encontradas(peliculas)
 
     def __mostrar_peliculas_encontradas(self, peliculas):
         self.__ui.table_peliculas_actores.setRowCount(0)
@@ -86,17 +66,7 @@ class VentanaActor(QDialog):
             self.__ui.table_peliculas_actores.insertRow(fila)
             self.__ui.table_peliculas_actores.setItem(fila, 0, QTableWidgetItem(pelicula["titulo"]))
 
-        self.__ui.table_peliculas_actores.itemDoubleClicked.connect(self.__cargar_detalles_pelicula)
+        self.__ui.table_peliculas_actores.itemDoubleClicked.connect(self.emitir_abrir_pelicula)
 
-    def __cargar_detalles_pelicula(self, item):
-        titulo = item.text()
-        pelicula = self.__buscar_pelicula_por_titulo(titulo)
-        if pelicula:
-            ventana_pelicula = VentanaPelicula(pelicula)
-            ventana_pelicula.exec()
-
-    def __buscar_pelicula_por_titulo(self, titulo):
-        for pelicula in self.__peliculas:
-            if pelicula["titulo"] == titulo:
-                return pelicula
-        return None
+    def emitir_abrir_pelicula(self, item):
+        self.abrir_pelicula.emit(item.text())
