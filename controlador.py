e62f0a97b6967535a17f12a107c4c1e3f6aed6b0
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2025-03-19 17:25:16 -0300

    Las busquedas las piden al catalogo

--- controlador.py
+++ controlador.py
@@ -1,60 +1,63 @@
-from modelo import Pelicula, Actor
+from modelos.modelo import Catalogo
 from vista.ventana_principal import VentanaPrincipal
 from vista.ventana_pelicula import VentanaPelicula
 from vista.ventana_actores import VentanaActor
 
-
 class Controlador:
     def __init__(self):
-        self.__modelo_pelicula = Pelicula()
-        peliculas = self.__modelo_pelicula.obtener_peliculas()
+        self.__catalogo = Catalogo()
+
+        # Instancias Ventana Principal
+        peliculas = self.__catalogo.obtener_peliculas()
         self.__ventana_principal = VentanaPrincipal()
         self.__ventana_principal.cargar_peliculas(peliculas)
         self.__ventana_principal.buscar.connect(self.__buscar_peliculas)
         self.__ventana_principal.abrir_pelicula.connect(self.__mostrar_pelicula)
         self.__ventana_principal.abrir_ventana_busqueda_actores.connect(self.__abrir_ventana_busqueda_actores)
 
+        # Instancias Ventana Película
         self.__ventana_pelicula = VentanaPelicula()
 
-        self.__modelo_actor = Actor()
+        # Instancias Ventana Actores
         self.__ventana_actores = VentanaActor()
         self.__ventana_actores.buscar_actor.connect(self.__buscar_actor)
         self.__ventana_actores.busqueda_por_actores.connect(self.__buscar_por_actores)
         self.__ventana_actores.abrir_pelicula.connect(self.__mostrar_pelicula)
 
-
     def __buscar_peliculas(self, titulo):
-        peliculas = self.__modelo_pelicula.buscar_por_titulo(titulo)
-
+        peliculas = self.__catalogo.buscar_peliculas_por_titulo(titulo)
         if len(peliculas) == 0:
             self.__ventana_principal.mostrar_error("No se encontró la película")
-            peliculas = self.__modelo_pelicula.obtener_peliculas()
+            peliculas = self.__catalogo.obtener_peliculas()
 
         self.__ventana_principal.cargar_peliculas(peliculas)
 
     def __mostrar_pelicula(self, titulo):
-        pelicula = self.__modelo_pelicula.obtener_por_titulo(titulo)
-        self.__ventana_pelicula.mostrar_datos(pelicula)
-        self.__ventana_pelicula.exec()
+        peliculas = self.__catalogo.buscar_peliculas_por_titulo(titulo)
+
+        if peliculas:
+            pelicula = peliculas[0]
+            self.__ventana_pelicula.mostrar_datos(pelicula)
+            self.__ventana_pelicula.exec()
+        else:
+            self.__ventana_principal.mostrar_error("No se encontró la película.")
 
     def __abrir_ventana_busqueda_actores(self):
-        self.__ventana_actores.cargar_actores(self.__modelo_actor.obtener_todos())
+        actores = self.__catalogo.obtener_actores_unicos()
+        self.__ventana_actores.cargar_actores(actores)
         self.__ventana_actores.exec()
 
     def __buscar_actor(self, nombre):
-        actor = self.__modelo_actor.buscar_actor_por_nombre(nombre)
-
-        if len(actor) == 0:
+        actores = self.__catalogo.obtener_actor_por_nombre(nombre)
+        if len(actores) == 0:
             self.__ventana_actores.mostrar_error("No se encontró el actor")
-            self.__ventana_actores.cargar_actores(self.__modelo_actor.obtener_todos())
-
         else:
-            self.__ventana_actores.cargar_actores(actor)
+            self.__ventana_actores.cargar_actores(actores)
 
     def __buscar_por_actores(self, actor_n1, actor_n2):
         if actor_n1 and actor_n2:
             if actor_n1 != actor_n2:
-                peliculas_encontradas = self.__modelo_actor.buscar_actores_en_pelicula(actor_n1, actor_n2)
+                peliculas_encontradas = self.__catalogo.buscar_peliculas_por_actores(actor_n1, actor_n2)
 
                 if len(peliculas_encontradas) == 0:
                     self.__ventana_actores.mostrar_error("No se encontraron películas con esos actores.")
@@ -67,5 +70,3 @@ class Controlador:
 
     def ejecutar(self):
         self.__ventana_principal.show()
-
-
